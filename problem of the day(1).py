d117319d88d65164ef2d9cf8125d39cf53d39c60
Maathar <130396868+Maathar@users.noreply.github.com>
2023-05-09 11:23:33 +0400

    Add files via upload

---
+++ problem of the day(1).py
@@ -0,0 +1,11 @@
+number1=int (input("Enter number1="))
+number2=int (input("Enter number2="))
+number3=int (input("Enter number3="))
+
+Max_number=max(number1,number2,number3)
+
+if number1==number2==number3:
+    print("All number are equal")
+
+else:
+    print(Max_number)
