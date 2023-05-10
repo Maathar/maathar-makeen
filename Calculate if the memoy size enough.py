5dc9da23a6acdcfd21e97b65706d06dffbb8a2f8
Maathar <130396868+Maathar@users.noreply.github.com>
2023-05-10 09:48:24 +0400

    Add files via upload
    The input as constant

---
+++ Calculate if the memoy size enough.py
@@ -0,0 +1,12 @@
+memory_size=20
+file_size=12
+number_of_file=2
+size_of_files=number_of_file*file_size
+
+
+if size_of_files<memory_size:
+    print("The size is enough")
+    
+else:
+    print("The size is not enough")
+    
