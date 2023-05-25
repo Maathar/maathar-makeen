f96cc9dbeae2c23678de10639f4365e3089fdf2e
Maathar <130396868+Maathar@users.noreply.github.com>
2023-05-25 13:33:01 +0400

    Add files via upload

---
+++ problem of the day(5).py
@@ -0,0 +1,15 @@
+for i in range(1, 6):
+    print(" " * (5 - i), end="")
+    print("*" * i, end="")
+    print(" " *5, end="")
+    print("*" * i)
+
+ 
+print("*****- - -*****")
+print("*****- - -*****")
+
+for i in range(5, 0, -1):
+    print(" " * (5 - i), end="")
+    print("*" * i, end="")
+    print(" " * 5, end="")
+    print("*" * i)
